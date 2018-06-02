26c0f1836126cfdb03adde9247260a041bdbd074
mrjoel <joel1994@users.noreply.github.com>
2018-06-02 21:13:12 +0800

    Update index.php

--- tool/index.php
+++ tool/index.php
@@ -190,5 +190,14 @@
 	<script src="https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
 	<script src="./layui/layui.js"></script>
 	<script src = "./static/embed.js?v=1.5"></script>
+	<!-- statistics -->
+   <script>
+   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+   ga('create', 'UA-82121763-1', 'auto');
+   ga('send', 'pageview');
+   </script>
 </body>
 </html>
