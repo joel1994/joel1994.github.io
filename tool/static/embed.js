0fd237be2d9718281e6a905e30277bc4cc077911
mrjoel <joel1994@users.noreply.github.com>
2018-09-21 11:11:06 +0800

    Update embed.js

--- tool/static/embed.js
+++ tool/static/embed.js
@@ -112,7 +112,7 @@ function mobile(){
 	var host = window.location.host;
 	var pathname = window.location.pathname;
 	var url = protocol + '//' + host + pathname;
-	var qrcode = "https://www.kuaizhan.com/common/encode-png?large=true&data=" + url;
+	var qrcode = "https://him.cm/tool/qrcreat/?text=" + url;
 	var qrimg = "<center><img src = '" + qrcode + "' /></center>";
 	layer.open({
 		type: 1,
